73d2c54da5bbff488322c7c39f59a7596e1390c6
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2022-06-28 18:10:40 +0200

    checker_tile_item

--- tile_item.cpp
+++ tile_item.cpp
@@ -1,8 +1,9 @@
+#include <string>
 #include <boost/gil.hpp>
 #include <QPainter>
 #include "tile_stats.hpp"
 #include "tile_item.hpp"
-using std::move, std::unique_ptr;
+using std::move, std::unique_ptr, std::string, std::to_string;
 using boost::gil::interleaved_view, boost::gil::gray16_view_t, boost::gil::gray16_pixel_t;
 
 tile_item::tile_item(tiff_tiled_file const & tiff, size_t tile_idx) {
@@ -12,6 +13,18 @@ tile_item::tile_item(tiff_tiled_file const & tiff, size_t tile_idx) {
 
 	// create QPixmap
 	QImage tile_img(reinterpret_cast<uint8_t *>(tile_data.get()), tile_w, tile_w, sizeof(uint16_t)*tile_w, QImage::Format_Grayscale16);
+
+	// TODO: add tile index (this canbe only done for RGB images :(
+	{
+		QPainter painter{&tile_img};
+		QPen pen;
+		pen.setWidth(20);
+		pen.setColor(Qt::red);
+		painter.setPen(pen);
+		painter.drawText(0, 0, "hello");
+		painter.end();
+	}
+
 	_pixels = QPixmap::fromImage(move(tile_img));  // TODO: check whether _pixels are data owner
 
 	if (!_checker_replacement) {  // print stats for tiff tiles
@@ -27,4 +40,37 @@ QRectF tile_item::boundingRect() const {
 
 void tile_item::paint(QPainter * paint, QStyleOptionGraphicsItem const * option, QWidget * widget) {
 	paint->drawPixmap(0, 0, _pixels.width(), _pixels.height(), _pixels);
+}
+
+checker_tile_item::checker_tile_item(size_t tile_idx) {
+	// get tile data
+	size_t const tile_w = 256;  // in pixels
+
+	// create QPixmap
+	QImage tile_img(tile_w, tile_w, QImage::Format_ARGB32_Premultiplied);
+
+	// TODO: populate with checker pattern
+
+	draw_tile_idx(tile_idx, tile_img);
+
+	_pixels = QPixmap::fromImage(move(tile_img));  // TODO: check whether _pixels are data owner
+}
+
+QRectF checker_tile_item::boundingRect() const {
+	return QRectF(0, 0, _pixels.width(), _pixels.height());
+}
+
+void checker_tile_item::paint(QPainter * paint, QStyleOptionGraphicsItem const * option, QWidget * widget) {
+	paint->drawPixmap(0, 0, _pixels.width(), _pixels.height(), _pixels);
+}
+
+void checker_tile_item::draw_tile_idx(size_t tile_idx, QImage & tile) {
+	QPainter painter{&tile};
+	QPen pen;
+	pen.setWidth(20);
+	pen.setColor(Qt::red);
+	painter.setPen(pen);
+	string text = to_string(tile_idx);
+	painter.drawText(tile.width()/2, tile.height()/2, text.c_str());
+	painter.end();
 }
--- tile_item.hpp
+++ tile_item.hpp
@@ -14,3 +14,17 @@ private:
 
 	bool _checker_replacement = true;
 };
+
+//! Checker tile wiht tile index for testing purpose.
+class checker_tile_item : public QGraphicsItem {
+public:
+	//! \param image Landsat 8 image.
+	checker_tile_item(size_t tile_idx);
+	void paint(QPainter * paint, QStyleOptionGraphicsItem const * option, QWidget * widget) override;
+	QRectF boundingRect() const override;
+
+private:
+	void draw_tile_idx(size_t tile_idx, QImage & tile);
+
+	QPixmap _pixels;
+};
