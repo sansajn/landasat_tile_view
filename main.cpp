667115a385a463a9765bdb9fa7c59e9a61f694c9
Adam Hlavatovic <adam.hlavatovic@protonmail.ch>
2022-06-29 16:50:59 +0200

    partial work

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 #include "tile_item.hpp"
 #include "tile_grid.hpp"
 #include "tiff_tiled_file.hpp"
+#include "map_view.hpp"
 
 using std::array, std::vector, std::pair, std::byte;
 using std::move, std::unique_ptr;
@@ -34,53 +35,12 @@ constexpr int default_zvalue = 100;
 
 class map_graphics_view : public QGraphicsView {  // TODO: move to map_view module
 public:
-	map_graphics_view(size_t width, size_t height, tiff_tiled_file const & tiff, QGraphicsScene & scene) {
-		// populate view by tile items
-		size_t const tile_count_x = ceil((float)width / tile_size),  // TODO: use more tiles to suppor smooth scrolling
-			tile_count_y = ceil((float)height / tile_size);
-
-		_tiles.reserve(tile_count_y * tile_count_y);
-
-		for (size_t y = 0; y < tile_count_y; ++y) {
-			for (size_t x = 0; x < tile_count_x; ++x) {
-				index_tile_item * tile = new index_tile_item{300 + y*tile_count_x + x};  // TODO: handle tile-idx
-				_tiles.push_back(tile);
-				QTransform T;
-				T.translate(x * tile_size, y * tile_size);
-				tile->setTransform(T);  // TODO: can we create translate transformation by kind of translated() function?
-				scene.addItem(tile);
-			}
-		}
-	}
+	map_graphics_view(size_t width, size_t height, tiff_tiled_file const & tiff, QGraphicsScene & scene);
 
 protected:
-	void mouseMoveEvent(QMouseEvent * event) override {  //!< \note left/right/midddle mouse button must be pressed to receive event
-		QPoint pos = event->pos();  // pozicia vo widgete
-		QPointF screen_pos = event->screenPos();
-		QPointF position = event->position();
-		spdlog::info("mouse moved: view-pos=({},{}), screen-pos=({},{}), position=({},{})", pos.x(), pos.y(), screen_pos.x(), screen_pos.y(), position.x(), position.y());
-
-		QPointF dp = event->position() - _mouse_click_pos;
-		if (dp != QPointF{}) {
-			pan_by(dp);
-			_mouse_click_pos = event->position();
-		}
-
-		QGraphicsView::mouseMoveEvent(event);
-	}
-
-	void mousePressEvent(QMouseEvent * event) override {
-		QPointF local_pos = event->localPos();
-		spdlog::info("mouse button clicked at: local-pos=({},{})", local_pos.x(), local_pos.y());
-
-		_mouse_click_pos = event->localPos();
-
-		QGraphicsView::mousePressEvent(event);
-	}
-
-	void mouseReleaseEvent(QMouseEvent * event) override {
-		QGraphicsView::mouseReleaseEvent(event);
-	}
+	void mouseMoveEvent(QMouseEvent * event) override;  //!< \note left/right/midddle mouse button must be pressed to receive event
+	void mousePressEvent(QMouseEvent * event) override;
+	void mouseReleaseEvent(QMouseEvent * event) override;
 
 private:
 	void pan_by(QPointF pos);  //!< Translate/pan view by \c pos position.
@@ -88,6 +48,54 @@ private:
 	QPointF _mouse_click_pos;
 	vector<index_tile_item *> _tiles;  //!< list of tiles visible in view
 };
+
+
+map_graphics_view::map_graphics_view(size_t width, size_t height, tiff_tiled_file const & tiff, QGraphicsScene & scene) {
+	// populate view by tile items
+	size_t const tile_count_x = ceil((float)width / tile_size),  // TODO: use more tiles to suppor smooth scrolling
+		tile_count_y = ceil((float)height / tile_size);
+
+	_tiles.reserve(tile_count_y * tile_count_y);
+
+	for (size_t y = 0; y < tile_count_y; ++y) {
+		for (size_t x = 0; x < tile_count_x; ++x) {
+			index_tile_item * tile = new index_tile_item{300 + y*tile_count_x + x};  // TODO: handle tile-idx
+			_tiles.push_back(tile);
+			QTransform T;
+			T.translate(x * tile_size, y * tile_size);
+			tile->setTransform(T);  // TODO: can we create translate transformation by kind of translated() function?
+			scene.addItem(tile);
+		}
+	}
+}
+
+void map_graphics_view::mouseMoveEvent(QMouseEvent * event) {
+	QPoint pos = event->pos();  // pozicia vo widgete
+	QPointF screen_pos = event->screenPos();
+	QPointF position = event->position();
+	spdlog::info("mouse moved: view-pos=({},{}), screen-pos=({},{}), position=({},{})", pos.x(), pos.y(), screen_pos.x(), screen_pos.y(), position.x(), position.y());
+
+	QPointF dp = event->position() - _mouse_click_pos;
+	if (dp != QPointF{}) {
+		pan_by(dp);
+		_mouse_click_pos = event->position();
+	}
+
+	QGraphicsView::mouseMoveEvent(event);
+}
+
+void map_graphics_view::mousePressEvent(QMouseEvent * event) {
+	QPointF local_pos = event->localPos();
+	spdlog::info("mouse button clicked at: local-pos=({},{})", local_pos.x(), local_pos.y());
+
+	_mouse_click_pos = event->localPos();
+
+	QGraphicsView::mousePressEvent(event);
+}
+
+void map_graphics_view::mouseReleaseEvent(QMouseEvent * event) {
+	QGraphicsView::mouseReleaseEvent(event);
+}
 
 
 void map_graphics_view::pan_by(QPointF pos) {
